dd80393facf51f11f6d9f07fd4a2bef3a2497da8
Darvand <david.montoya@wolox.co>
2018-11-26 11:07:19 -0500

    Added exports in errors.js, that will handle the create user error.

--- app/errors.js
+++ app/errors.js
@@ -5,3 +5,10 @@ const internalError = (message, internalCode) => ({
 
 exports.DEFAULT_ERROR = 'default_error';
 exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
+
+exports.CREATE_USER_ERROR = 'create_user_error';
+exports.createUserError = errors => {
+  const handledErrors = [];
+  errors.map(({ message }) => handledErrors.push(message));
+  return internalError(handledErrors, exports.CREATE_USER_ERROR);
+};
