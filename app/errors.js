4ea9b6d09cda083ff92363afb1e9033d4c557d71
Darvand <david.montoya@wolox.co>
2018-11-27 08:45:22 -0500

    The user validation errors are in an array, so, this error method need to send an array too.

--- app/errors.js
+++ app/errors.js
@@ -15,5 +15,5 @@ exports.CREATE_USER_ERROR = 'create_user_error';
 exports.createUserError = error => {
   logger.error(error.original);
   if (error.original.code !== '23505') return exports.defaultError('Unexpected data base error');
-  return internalError('The email must be unique', exports.CREATE_USER_ERROR);
+  return internalError(['The email must be unique'], exports.CREATE_USER_ERROR);
 };
