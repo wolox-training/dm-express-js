0e08cf8e4840cb69a5e097b4ea985ee1eeaa0e1c
Darvand <david.montoya@wolox.co>
2018-11-27 09:50:19 -0500

    Simplified the errors file. 'invalidUserError' deleted.

--- app/errors.js
+++ app/errors.js
@@ -8,12 +8,5 @@ const internalError = (message, internalCode) => ({
 exports.DEFAULT_ERROR = 'default_error';
 exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
 
-exports.INVALID_USER_ERROR = 'invalid_user_error';
-exports.invalidUserError = messages => internalError(messages, exports.INVALID_USER_ERROR);
-
 exports.CREATE_USER_ERROR = 'create_user_error';
-exports.createUserError = error => {
-  logger.error(error.original);
-  if (error.original.code !== '23505') return exports.defaultError('Unexpected data base error');
-  return internalError('The email must be unique', exports.CREATE_USER_ERROR);
-};
+exports.createUserError = messages => internalError(messages, exports.CREATE_USER_ERROR);
