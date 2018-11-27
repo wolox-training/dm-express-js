10d49c10c5c5447590cc7f765a6cb3bda2c8384e
Darvand <david.montoya@wolox.co>
2018-11-27 11:52:46 -0500

    Added method authenticate, that will check if the credentials are OK. If yes, it will create the token.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,5 +1,6 @@
 const logger = require('../logger'),
   bcrypt = require('bcryptjs'),
+  sessionManager = require('../services/sessionManager'),
   User = require('../models').users,
   errors = require('../errors');
 
@@ -13,4 +14,25 @@ exports.create = ({ user }, response, next) => {
       logger.error(error);
       response.status(400).send(errors.defaultError('User creation fail'));
     });
+};
+
+exports.authenticate = (request, response, next) => {
+  const { email, password } = request.body;
+  User.findByEmail(email).then(user => {
+    if (user) {
+      bcrypt.compare(password, user.password).then(isValid => {
+        if (isValid) {
+          const authentification = sessionManager.encode({ email: user.email });
+          response
+            .status(200)
+            .set(sessionManager.HEADER_NAME, authentification)
+            .send(user);
+        } else {
+          response.status(400).send('The password is not correct');
+        }
+      });
+    } else {
+      response.status(400).send('The email is not correct');
+    }
+  });
 };
