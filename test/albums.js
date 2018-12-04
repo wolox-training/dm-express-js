783679f22d705891d56d9e5add59a0d1538c669f
Darvand <david.montoya@wolox.co>
2018-12-04 11:21:43 -0500

    Added new test cases.

--- test/albums.js
+++ test/albums.js
@@ -37,6 +37,20 @@ const handleError = (error, expectedMessage, expectedInternalCode) => {
   internalCode.should.equal(expectedInternalCode);
 };
 
+const admin = 'admin@wolox.co';
+const regular = 'unique@wolox.co';
+const fetch = (email, method = 'post', endpoint) =>
+  chai
+    .request(server)
+    .post('/users/sessions')
+    .send({ email, password: '123456789' })
+    .then(loggedResponse =>
+      chai
+        .request(server)
+        [method](endpoint)
+        .set(sessionManager.HEADER_NAME, loggedResponse.headers[sessionManager.HEADER_NAME])
+    );
+
 describe('users', () => {
   beforeEach(() => {
     nock('https://jsonplaceholder.typicode.com')
@@ -75,21 +89,8 @@ describe('users', () => {
     });
   });
   describe('/albums/:id POST', () => {
-    const admin = 'admin@wolox.co';
-    const regular = 'unique@wolox.co';
-    const fetch = (email, method = 'post', endpoint) =>
-      chai
-        .request(server)
-        .post('/users/sessions')
-        .send({ email, password: '123456789' })
-        .then(loggedResponse =>
-          chai
-            .request(server)
-            [method](`/albums${endpoint}`)
-            .set(sessionManager.HEADER_NAME, loggedResponse.headers[sessionManager.HEADER_NAME])
-        );
-    it('should sucess when buying a new album', done => {
-      fetch(admin, 'post', `/${resMocked[0].id}`)
+    it('should success when buying a new album', done => {
+      fetch(admin, 'post', `/albums/${resMocked[0].id}`)
         .then(res => {
           res.should.have.status(200);
           const { idAlbum, idUser } = res.body;
@@ -100,12 +101,12 @@ describe('users', () => {
         .then(() => done());
     });
     it('should fail because album does not exist', done => {
-      fetch(admin, 'post', `/5`)
+      fetch(admin, 'post', `/albums/5`)
         .catch(error => handleError(error, 'There is not album with id 5', errors.BUY_ALBUM_ERROR))
         .then(() => done());
     });
     it('should fail because user alredy bougth the album', done => {
-      fetch(regular, 'post', `/1`)
+      fetch(regular, 'post', `/albums/1`)
         .catch(error => handleError(error, 'The user already bought this album', errors.BUY_ALBUM_ERROR))
         .then(() => done());
     });
@@ -114,6 +115,57 @@ describe('users', () => {
         .request(server)
         .post('/albums/1')
         .catch(error => handleError(error, 'You must be logged to buy albums', errors.AUTHENTICATION_ERROR))
+        .then(() => done());
+    });
+  });
+
+  describe('/users/:user_id/albums GET', () => {
+    const endpoint = userId => `/users/${userId}/albums`;
+    it('should fail because user not logged', done => {
+      chai
+        .request(server)
+        .get(endpoint(1))
+        .catch(error =>
+          handleError(error, 'You must be logged to see the purchased albums', errors.AUTHENTICATION_ERROR)
+        )
+        .then(() => done());
+    });
+    it('should fail because the user is trying to find albums from other user', done => {
+      fetch(regular, 'get', endpoint(12))
+        .catch(error =>
+          handleError(
+            error,
+            'You are not allowed to see albums purchased from other users',
+            errors.LIST_ALBUM_ERROR
+          )
+        )
+        .then(() => done());
+    });
+    it('should success and return a message telling that the user does not purchased any album', done => {
+      fetch(admin, 'get', endpoint(12))
+        .then(res => {
+          res.should.have.status(200);
+          res.text.should.equal('The user has not bought any album yet');
+          dictum.chai(res);
+        })
+        .then(() => done());
+    });
+    it('should success when listing albums from a user as an admin', done => {
+      fetch(admin, 'get', endpoint(11))
+        .then(res => {
+          res.should.have.status(200);
+          res.body[0].should.deep.equal(resMocked[0]);
+          dictum.chai(res);
+        })
+        .then(() => done());
+    });
+    it('should success when listing albums from self', done => {
+      fetch(regular, 'get', endpoint(11))
+        .then(res => {
+          res.should.have.status(200);
+          res.body[0].should.deep.equal(resMocked[0]);
+          dictum.chai(res);
+        })
         .then(() => done());
     });
   });
